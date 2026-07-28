5834d555605b748332a61ea8f8bd3dd731529d05
R8152 <raulteles05@gmail.com>
2026-07-27 21:02:13 -0300

    style: mudar o header

--- plataformaUniversitaria/src/components/Header.jsx
+++ plataformaUniversitaria/src/components/Header.jsx
@@ -1,4 +1,21 @@
+import Perfil from '../images/Perfil.jpg';
+import Sino from '../images/Sinonotificar.jpg';
 import "./Header.css"
-function Header() {
+function Header(btn) {
+    return (
+        <header className="Header-body">
+            <h1 className="Header-name">Unitroca</h1>
+            <main className="Header-options">
+                <span className="option1">Início</span>
+                <span className="option2">Impacto</span>
+                <span className="option3">Sobre</span>
+            </main>
+            <main className="notificar_perfil">
+                <img src={Sino} alt="" className='sino'/>
+                <img src={Perfil} alt="" className='perfil'/>
+            </main>
+        </header>
+    )
     
 }
+export default Header
