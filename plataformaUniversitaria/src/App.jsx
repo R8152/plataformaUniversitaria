e8ed4be0201f19ce5df0162d2f4618fae40d36bf
R8152 <raulteles05@gmail.com>
2026-07-28 20:04:38 -0300

    chore: instala Section2

--- plataformaUniversitaria/src/App.jsx
+++ plataformaUniversitaria/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Section from './components/Section'
-
+import Section2 from './components/Section2'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +10,7 @@ function App() {
     <>
       <Header></Header>
       <Section></Section>
+      <Section2></Section2>
     </>
   )
 }
---
+++ plataformaUniversitaria/src/components/Section2.jsx
@@ -0,0 +1,9 @@
+import "./Section2.css"
+function Section2(){
+    return(
+        <section className="Section2">
+
+        </section>
+    )
+}
+export default Section2
