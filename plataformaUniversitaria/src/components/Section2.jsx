0b970ff8e14327d265e6085b32e8f34b1c646865
R8152 <raulteles05@gmail.com>
2026-07-28 21:07:03 -0300

    style: estiliza a Section2

--- plataformaUniversitaria/src/components/Section2.jsx
+++ plataformaUniversitaria/src/components/Section2.jsx
@@ -1,8 +1,25 @@
 import "./Section2.css"
-function Section2(){
+function Section2(btn){
     return(
         <section className="Section2">
-
+            <main className="itens-trocados">
+                <img src="" alt="" className="Balde"/>
+                <h1 className="title-main">1450+</h1>
+                <h2 className="subtitle-main">Itens Trocados</h2>
+                <span className="info-main">Livros e equipamentos que ganharam <p>uma nova vida útil este semestre.</p></span>
+            </main>
+            <main className="co2-evitado">
+                <img src="" alt="" className="Folha"/>
+                <h1 className="title-main">12.4t</h1>
+                <h2 className="subtitle-main">CO2 Evitado</h2>
+                <span className="info-main">Impacto ambiental reduzido através da <p>reutilização de eletrônicos e papel.</p></span>
+            </main>
+            <main className="dinheiro-aluno">
+                <img src="" alt="" className="Money"/>
+                <h1 className="title-main">R$ 85k+</h1>
+                <h2 className="subtitle-main">Economia Aluna</h2>
+                <span className="info-main">Total economizado pelos estudantes ao <p>optar pela economia circular.</p></span>
+            </main>
         </section>
     )
 }
