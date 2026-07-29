5f8139b4152f84ed31c7a508229e618775bc5f99
R8152 <raulteles05@gmail.com>
2026-07-29 09:58:22 -0300

    style: finaliza estilização de section2

--- plataformaUniversitaria/src/components/Section2.jsx
+++ plataformaUniversitaria/src/components/Section2.jsx
@@ -1,21 +1,24 @@
 import "./Section2.css"
+import Balde from "../images/balde_troca.png"
+import Folha from "../images/folha_co.png"
+import Money from "../images/dinheiro_aluno.png"
 function Section2(btn){
     return(
         <section className="Section2">
             <main className="itens-trocados">
-                <img src="" alt="" className="Balde"/>
+                <img src={Balde} alt="" className="Balde"/>
                 <h1 className="title-main">1450+</h1>
                 <h2 className="subtitle-main">Itens Trocados</h2>
                 <span className="info-main">Livros e equipamentos que ganharam <p>uma nova vida útil este semestre.</p></span>
             </main>
             <main className="co2-evitado">
-                <img src="" alt="" className="Folha"/>
+                <img src={Folha} alt="" className="Folha"/>
                 <h1 className="title-main">12.4t</h1>
                 <h2 className="subtitle-main">CO2 Evitado</h2>
                 <span className="info-main">Impacto ambiental reduzido através da <p>reutilização de eletrônicos e papel.</p></span>
             </main>
             <main className="dinheiro-aluno">
-                <img src="" alt="" className="Money"/>
+                <img src={Money} alt="" className="Money"/>
                 <h1 className="title-main">R$ 85k+</h1>
                 <h2 className="subtitle-main">Economia Aluna</h2>
                 <span className="info-main">Total economizado pelos estudantes ao <p>optar pela economia circular.</p></span>
