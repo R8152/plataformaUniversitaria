534880a5b7476ea54ff98f717c664c60bf8b2486
R8152 <raulteles05@gmail.com>
2026-07-30 11:52:48 -0300

    style: estiliza EmailPart

--- plataformaUniversitaria/src/components/EmailPart.jsx
+++ plataformaUniversitaria/src/components/EmailPart.jsx
@@ -2,7 +2,13 @@ import "./EmailPart.css"
 function EmailPart(){
     return(
         <section className="EmailPart">
-
+            <h1 className="title">Pronto para circular?</h1>
+            <span className="infoEmail">Cadastre-se com seu e-mail institucional e comece a trocar hoje <p>mesmo.</p></span>
+            <div className="emailcomeco">
+                <span className="email">seuemail@universidade.edu</span>
+                <button className="comeco">Começar Grátis</button>
+            </div>
+            <span className="exclusive">Exclusivo para estudantes e professores verificados.</span>
         </section>
     )
 }
