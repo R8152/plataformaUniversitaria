12ef36c3159650fff29f33c6ad95c5c75e346a63
R8152 <raulteles05@gmail.com>
2026-08-05 16:14:06 -0300

    style: estiliza footer

--- plataformaUniversitaria/src/components/Footer.jsx
+++ plataformaUniversitaria/src/components/Footer.jsx
@@ -2,22 +2,33 @@ function Footer(){
     return(
         <>
         <footer>
-            <main className="info_footer">
-                <div>
-                    <h1>UniTroca</h1>
-                    <span><p>A plataforma líder em economia</p><p>circular acadêmica no Brasil.</p> <p>Construindo um futuro sustentável</p> <p>através da colaboração estudantil.</p></span>
+            <main className="footer1">
+                <div className="info1">
+                    <h1 className="footerh1">UniTroca</h1>
+                    <span className=""><p>A plataforma líder em economia</p><p>circular acadêmica no Brasil.</p> <p>Construindo um futuro sustentável</p> <p>através da colaboração estudantil.</p></span>
                 </div>
-                <div>
 
+                <div className="info2">
+                    <h3>Plataforma</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-                <div>
 
+                <div className="info3">
+                    <h3>Categorias</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-                <div>
 
+                <div className="info4">
+                    <h3>Newsletter</h3>
                 </div>
             </main>
-            <main className="">
+            <main className="socialmedia">
 
             </main>
         </footer>
